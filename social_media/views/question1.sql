c42bae80cf94191801bbec92277e258552f6e740
NyAina70 <107650265+NyAina70@users.noreply.github.com>
2022-08-04 11:03:28 +0200

    Add files via upload

---
+++ social_media/views/question1.sql
@@ -0,0 +1,11 @@
+CREATE VIEW invitation_non_accepteid1 AS 
+
+SELECT first_name, last_name, nickname, is_accepted 
+from account 
+inner join send_invite on account.id_account = send_invite.id_account_receiver 
+where id_account = 1 and is_accepted = false;
+
+ --first_name | last_name | nickname | is_accepted
+--------------+-----------+----------+-------------
+-- Heaven     | Pfeffer   | Heave    | f
+-- (1 ligne)
